5990736417d9dae4c981eb69151280c702992c25
vladsmailov <vladislav.smailov@yandex.ru>
2023-04-16 14:57:14 +0500

    logging added in base_app functions.

--- base_app.py
+++ base_app.py
@@ -1,7 +1,15 @@
+import sys
+
+from loguru import logger
+
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+
+
+logger.add(sys.stderr, format="{time} {level} {message}", colorize=True, filter="my_module", level="INFO") # noqa: 503
+new_level = logger.level("START", no=38, color="<yellow>")
 
 
 class BasePage:
@@ -10,40 +18,51 @@ class BasePage:
         self.driver = driver
         self.base_url = "https://ya.ru/"
 
+    @logger.catch
     def find_element(self, locator, time=10):
         return WebDriverWait(self.driver, time).until(
             EC.presence_of_element_located(locator),
             message=f"Can't find element by locator {locator}"
         )
 
+    @logger.catch
     def find_elements(self, locator, time=10):
         return WebDriverWait(self.driver, time).until(
             EC.presence_of_all_elements_located(locator),
             message=f"Can't find elements by locator {locator}"
         )
 
+    @logger.catch
     def go_to_site(self):
+        logger.log("START", "NEW SESSION STARTED.")
         return self.driver.get(self.base_url)
 
+    @logger.catch
     def go_to_window(self, number):
         window = self.driver.window_handles[number]
         return self.driver.switch_to.window(window)
 
+    @logger.catch
     def select_all_text(self):
+        logger.info("PRESSED BUTTON COMBINATION TO SELECT ALL TEXT IN TEXT AREA.")
         ActionChains(self.driver) \
             .key_down(Keys.CONTROL) \
             .send_keys('a') \
             .key_up(Keys.CONTROL) \
             .perform()
 
+    @logger.catch
     def copy_all_text(self):
+        logger.info("PRESSED BUTTON COMBINATION TO COPY SELECTED TEXT IN CLIPBOARD.")
         ActionChains(self.driver) \
             .key_down(Keys.CONTROL) \
             .send_keys('c') \
             .key_up(Keys.CONTROL) \
             .perform()
 
+    @logger.catch
     def press_escape(self):
+        logger.info("PRESSED ESCAPE BUTTON.")
         ActionChains(self.driver).\
             send_keys(Keys.ESCAPE)\
             .perform()
--- yandex_pages.py
+++ yandex_pages.py
@@ -6,8 +6,8 @@ from selenium.webdriver.common.keys import Keys
 
 from base_app import BasePage
 
-logger.add(sys.stderr, format="{time} {level} {message}", filter="my_module", level="INFO") # noqa: 503
-logger.add("file_{time}.log", level="TRACE", rotation="100 MB")
+logger.add(sys.stderr, format="{time} {level} {message}", colorize=True, filter="my_module", level="INFO") # noqa: 503
+logger.add("log_file.log", level="TRACE", rotation="5 MB")
 
 
 class YandexSearchLocators:
@@ -43,69 +43,70 @@ class YandexSearchLocators:
 class SearchHelper(BasePage):
     """Class for working with search queries."""
 
+    @logger.catch
     def get_search_field(self):
         """Check search field."""
         logger.info("SEARCH FIELD OBJECT RECIEVED.")
-        logger.complete()
         return self.find_element(
             YandexSearchLocators.LOCATOR_YANDEX_SEARCH_FIELD, time=3
         )
 
+    @logger.catch
     def get_images_header(self):
         """Get header for getting searching field."""
         logger.info("HEADER OBJECT ON THE IMAGE SEARCHING FIELD RECIEVED.")
-        logger.complete()
         return self.find_element(
             YandexSearchLocators.LOCATOR_YANDEX_IMAGES_HEADER, time=3
         )
 
+    @logger.catch
     def get_images_search_field(self, header):
         """Get images search field."""
         logger.info("IMAGE SEARCH FIELD RECIEVED.")
-        logger.complete()
         return header.find_element(
             By.XPATH,
             '/html/body/header/div/div[1]/div[2]/form/div[1]/span/span'
         )
 
+    @logger.catch
     def get_search_field_text(self, search_field):
         """Get search field text."""
         logger.info(
             f"SEARCH FIELD TEXT={search_field.get_attribute('value')}."
         )
-        logger.complete()
         return search_field.get_attribute('value')
 
+    @logger.catch
     def enter_word(self, search_field, word):
         """Enter word to the search field."""
         logger.info(f"TEXT={word} TYPED INTO SEARCH FIELD.")
-        logger.complete()
         search_field.send_keys(word)
         return search_field
 
+    @logger.catch
     def press_return_button(self, object):
         """Press Return button."""
         logger.info("RETURN BUTTON PRESSED.")
-        logger.complete()
         return object.send_keys(Keys.RETURN)
 
+    @logger.catch
     def get_suggestions(self):
         """Check the suggestion."""
         logger.info("SUGGESTIONS IN SEARCH FIELD RECIEVED.")
-        logger.complete()
         return self.find_element(
             YandexSearchLocators.LOCATOR_YANDEX_SUGGESTIONS, time=10
         )
 
+    @logger.catch
     def get_page_source(self):
         """Get the page source."""
         logger.info("PAGE SOURCE RECIEVED.")
-        logger.complete()
         searching_results = self.find_elements(
             YandexSearchLocators.LOCATOR_YANDEX_PAGE_SOURCE, time=2
         )
         return len(searching_results)
 
+    @logger.catch
     def get_first_link(self):
         """Get the first link."""
         first_link = self.find_element(
@@ -113,37 +114,36 @@ class SearchHelper(BasePage):
         )
         url = first_link.get_attribute(name='href')
         logger.info(f"FIRST LINK IN THE SEARCHING RESULT: {url}.")
-        logger.complete()
         return url
 
+    @logger.catch
     def get_menu_button(self):
         """Get the menu button."""
         logger.info("MENU BUTTON RECIEVED.")
-        logger.complete()
         return self.find_element(
             YandexSearchLocators.LOCATOR_YANDEX_MENU_BUTTON, time=3
         )
 
+    @logger.catch
     def get_scrollbar(self):
         """Get scrollar window, for getting service buttons."""
         logger.info("SCROLLBAR RECIEVED.")
-        logger.complete()
         return self.find_element(
             YandexSearchLocators.LOCATOR_YANDEX_SCROLLBAR, time=3
         )
 
+    @logger.catch
     def get_images_button(self, scrollbar):
         """Get images page."""
         logger.info("IMAGES BUTTON RECIEVED.")
-        logger.complete()
         return scrollbar.find_element(
             By.XPATH, "//a[contains(@href, 'https://yandex.ru/images/')]"
         )
 
+    @logger.catch
     def get_first_popular_images_category(self):
         """Choose first popular image ."""
         logger.info("FIRST POPULAR IMAGES CATEGORY RECIEVED.")
-        logger.complete()
         return self.find_element(
             YandexSearchLocators.LOCATOR_YANDEX_POPULAR_IMAGES_FIRST, time=5
         )
@@ -152,65 +152,64 @@ class SearchHelper(BasePage):
     def click_on(element):
         """Click on object."""
         logger.info("MOUSE LEFT CLICK.")
-        logger.complete()
         return element.click()
 
+    @logger.catch
     def switch_windows(self, number):
         """Switching browser window to next."""
         logger.info("BROWSER TAB WAS SWITCHED.")
-        logger.complete()
         return self.go_to_window(number)
 
+    @logger.catch
     def select_first_image(self, images):
         """Get first image from images search page."""
         logger.info("FIRST IMAGE FROM IMAGES-SEARCH RESULT RECIEVED.")
-        logger.complete()
         return images.find_element(
             By.XPATH, "//div[contains(@class, 'item_first')]"
         )
 
+    @logger.catch
     def get_image_bar(self):
         """Get images bar."""
         logger.info("IMAGES BAR RECIEVED.")
-        logger.complete()
         return self.find_element(
             YandexSearchLocators.LOCATOR_YANDEX_IMAGE_BAR, time=3
         )
 
+    @logger.catch
     def get_media_viewer(self):
         """Get media viewer for checking image existing."""
         logger.info("MEDIA VIEWER RECIEVED.")
-        logger.complete()
         return self.find_element(
             YandexSearchLocators.LOCATOR_YANDEX_MEDIA_VIEWER, time=3
         )
 
+    @logger.catch
     def get_image(self, media_viewer):
         """Get image object."""
         logger.info("IMAGE PREVIEW OBJECT RECIEVED.")
-        logger.complete()
         return media_viewer.find_element(
             By.XPATH, "//img[contains(@class, 'MMImage-Preview')]"
         )
 
+    @logger.catch
     def next_button(self, media_viewer):
         """Switch image to next."""
         logger.info("MEDIA VIEWER NEXT IMAGE BUTTON RECIEVED.")
-        logger.complete()
         return media_viewer.find_element(
             By.XPATH, "//div[contains(@class, 'LayoutMain')]"
         )
 
+    @logger.catch
     def previous_button(self, media_viewer):
         """Switch image to previous."""
         logger.info("MEDIA VIEWER PREVIOUS IMAGE BUTTON RECIEVED.")
-        logger.complete()
         return media_viewer.find_element(
             By.XPATH, "//div[contains(@class, 'MediaViewer-ButtonPrev')]"
         )
 
+    @logger.catch
     def get_url(self):
         """Get url of current page."""
         logger.info(f"CURRENT URL RECIEVED: {self.driver.current_url}.")
-        logger.complete()
         return self.driver.current_url
